38f17f5c4ef15b75c50ae3c62ac7c21778760ecb
hwaly <hwalybell@gmail.com>
2016-01-22 10:39:19 +0900

    Create PDO.class.php

---
+++ PDO.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PHP PDO MySQL Class
+ * https://github.com/hwaly/php-pdo-mysql-class
+ *
+ * @author DBROSCREATIVE
+ * @date 2016. 01 ~
+ * @version 0.1
+ *
+ * Licensed under the Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+ 
+class DB
+{
+  private $Host;
+  private $DBName;
+	private $DBUser;
+	private $DBPassword;
+	private $DBcharset = 'utf8';
+  
+  public function __construct($Host, $DBName, $DBUser, $DBPassword) {
+  }
+}
+ 
